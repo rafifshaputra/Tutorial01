8abf79775a7da20ad452b15242a85f0900301396
Kelompok 3 PPW A <kelompok3ppw@gmail.com>
2018-09-07 14:51:24 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,23 @@
+
+public class Mahasiswa {
+	int number;
+	String name;
+	
+	public Mahasiswa(int number, String name) {
+		super();
+		this.number = number;
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	
+
+}
