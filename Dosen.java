a5694cab43c50664c0c6363a5716e1742dca3f69
Kelompok 3 PPW A <kelompok3ppw@gmail.com>
2018-09-07 14:54:43 +0700

    Initial Commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,22 @@
+public class Dosen {
+	int number;
+	String name;
+	
+	public Dosen(int number, String name) {
+		super();
+		this.number = number;
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	
+
+}
